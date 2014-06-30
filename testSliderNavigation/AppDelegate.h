543836461164ea69a5bea78ff7e6401c286cbe3e
piyush123 <piyush@onor.net>
2014-06-29 18:31:07 -0700

    Initial Commit

---
+++ testSliderNavigation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testSliderNavigation
+//
+//  Created by piyush shah on 6/29/14.
+//  Copyright (c) 2014 onor inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
